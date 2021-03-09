2649398e3d50928a7930214f2dd0295b9802be4a
Konrad Ciepluch <konrad190008@wp.pl>
2021-03-09 14:59:15 +0100

    Add loops

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,15 @@
 const x = 5;
 let result = 0;
+
+for(let i=1; i<=x; i++){
+    result+=i;
+}
+console.log(result);
+// poniżej użycie z pętlą while
+/* let number = 1;
+
+while(number<=x) {
+    result+=number;
+    number++;
+}
+console.log(result); */
